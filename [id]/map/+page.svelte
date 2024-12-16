34b8f1f134583b156e78ee4e1e095c59d701766c
Roberto Valenti <robbyvalenti@gmail.com>
2024-12-16 10:38:23 +0100

    Revert "removed outsider [id]"
    This reverts commit 468cd1563aa8cbef0b25dc523307b64ebc835259.

---
+++ [id]/map/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let isViewingOrEditing = false; // or however you track the view/edit state
+</script>
+
+<Map 
+    selectedLens={selectedLens}
+    on:holonChange={() => {
+        isViewingOrEditing = false;
+    }} 
+/> 
