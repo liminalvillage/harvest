406222436582f8fa9fabede01d06c259664669d6
Roberto Valenti <robbyvalenti@gmail.com>
2025-08-21 15:52:48 +0200

    switched to ZeitCampBot login for testing

--- src/components/TelegramAuth.svelte
+++ src/components/TelegramAuth.svelte
@@ -63,7 +63,7 @@
 		// Create script element for Telegram Login Widget
 		const script = document.createElement('script');
 		script.src = 'https://telegram.org/js/telegram-widget.js?22';
-		script.setAttribute('data-telegram-login', 'HolonsBot');
+		script.setAttribute('data-telegram-login', 'ZeitCampBot');
 		script.setAttribute('data-size', 'large');
 		script.setAttribute('data-onauth', 'onTelegramAuth(user)');
 		script.setAttribute('data-request-access', 'write');
@@ -118,7 +118,7 @@
 		errorMessage = '';
 		
 		// The widget should handle this, but just in case
-		const botUsername = 'HolonsBot';
+		const botUsername = 'ZeitCampBot';
 		const loginUrl = `https://t.me/${botUsername}?start=login_${holonID}`;
 		
 		// Open Telegram login in a new window/tab as fallback
