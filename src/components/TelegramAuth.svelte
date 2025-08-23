07939c9182fe115ab5b1e8e3a87d37495092a7cd
Roberto Valenti <robbyvalenti@gmail.com>
2025-08-23 12:25:21 +0200

    Changing telegram auth domain name

--- src/components/TelegramAuth.svelte
+++ src/components/TelegramAuth.svelte
@@ -101,7 +101,7 @@
 			// Create script element for Telegram Login Widget
 			const script = document.createElement('script');
 			script.src = 'https://telegram.org/js/telegram-widget.js?22';
-			script.setAttribute('data-telegram-login', 'ZeitCampBot');
+			script.setAttribute('data-telegram-login', 'HolonsBot');
 			script.setAttribute('data-size', 'large');
 			script.setAttribute('data-onauth', 'onTelegramAuth(user)');
 			script.setAttribute('data-request-access', 'write');
